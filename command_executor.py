21dc4042ca4acdf7cf5f95eebeda9e645104d9aa
Anoopsmohan <anoop.sm@olacabs.com>
2018-02-11 17:19:38 +0530

    exception fix

--- command_executor.py
+++ command_executor.py
@@ -201,7 +201,6 @@ class ParkingLot(object):
             try:
                 msg = command(self, *params)
             except Exception as ex:
-                print(ex.message)
                 msg = "Invalid arguments"
         return msg
 
